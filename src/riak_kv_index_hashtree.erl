1cf51c553a2c0fa3d6f545d4f904092a2e402120
Joseph Blomstedt <joe@basho.com>
2013-02-08 02:44:20 -0800

    Testing: replace phash2 with hardcoded constant

--- src/riak_kv_index_hashtree.erl
+++ src/riak_kv_index_hashtree.erl
@@ -382,13 +382,8 @@ load_built(#state{trees=Trees}) ->
 
 %% Generate a hash value for a binary-encoded `riak_object'
 -spec hash_object(riak_object_t2b()) -> binary().
-hash_object(RObjBin) ->
-    %% Normalize the `riak_object' vector clock before hashing
-    RObj = binary_to_term(RObjBin),
-    Vclock = riak_object:vclock(RObj),
-    UpdObj = riak_object:set_vclock(RObj, lists:sort(Vclock)),
-    Hash = erlang:phash2(term_to_binary(UpdObj)),
-    term_to_binary(Hash).
+hash_object(_RObjBin) ->
+    <<42:64/integer>>.
 
 %% Fold over a given vnode's data, inserting each object into the appropriate
 %% hashtree. Use the `if_missing' option to only insert the key/hash pair if
